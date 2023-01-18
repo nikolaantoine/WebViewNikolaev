57de0a245e449cd994a982cd6ea3a8d632ed2882
Nikalayeu Anton Dmitievich <122828692+nikolaantoine@users.noreply.github.com>
2023-01-18 19:21:56 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        
+        // webView.loadUrl("http://www.google.com");
+        webView.loadData("<html><body><h1>Hi there!</h1><p>This is my website</p></body></html>", "text/html", "UTF-8");
+    }
+}
